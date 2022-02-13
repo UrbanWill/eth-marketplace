56d45a85afdacd1562a13580f9741935306a9eae
Will Urban <willian.urban@gmail.com>
2022-02-13 16:47:38 +0900

    configured absolute imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 
+import Test from "components/Test";
+
 const Home: NextPage = () => (
-  <p className="text-red-500">Hello Eth Marketplace!</p>
+  <>
+    <p className="text-red-500">Hello Eth Marketplace!</p>
+    <Test />
+  </>
 );
 
 export default Home;
