ec5c4d4da8a961c451ee2440b8b4289a2d504aba
Will Urban <willian.urban@gmail.com>
2022-03-13 16:53:55 +0900

    refactored button

--- components/ui/common/button/index.tsx
+++ components/ui/common/button/index.tsx
@@ -1,26 +1,46 @@
 import { FC } from "react";
 
+type Variants = "purple" | "red";
+
 interface Props {
   text: string;
   className?: string;
   disabled?: boolean;
   onHandleClick?: () => void;
+  isHoverable?: boolean;
+  variant?: Variants;
 }
 
-const Button: FC<Props> = ({ text, className, disabled, onHandleClick }) => (
-  <button
-    type="button"
-    disabled={disabled}
-    className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 border rounded-md text-base font-medium ${className}`}
-    onClick={onHandleClick}
-  >
-    {text}
-  </button>
-);
+const Button: FC<Props> = ({
+  text,
+  className,
+  disabled,
+  onHandleClick,
+  isHoverable,
+  variant = "purple",
+}) => {
+  const variants = {
+    purple: `text-white bg-indigo-600 ${isHoverable && "hover:bg-indigo-700"}`,
+    red: `text-white bg-red-600 ${isHoverable && "hover:bg-red-700"}`,
+  };
+
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 border rounded-md text-base font-medium ${className} ${variants[variant]}`}
+      onClick={onHandleClick}
+    >
+      {text}
+    </button>
+  );
+};
 
 Button.defaultProps = {
-  className: "text-white bg-indigo-600 hover:bg-indigo-700",
+  className: "",
   onHandleClick: () => {},
   disabled: false,
+  isHoverable: true,
+  variant: "purple",
 };
 export default Button;
