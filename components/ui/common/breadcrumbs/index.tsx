73b356079831979bc12798c2d2a18ddabfca2519
Will Urban <willian.urban@gmail.com>
2022-03-19 18:50:25 +0900

    refactored Breadcrumbs to take link array

--- components/ui/common/breadcrumbs/index.tsx
+++ components/ui/common/breadcrumbs/index.tsx
@@ -1,20 +1,34 @@
 import { FC } from "react";
 
-const Breadcrumbs: FC = () => (
+import Link from "next/link";
+
+interface ILink {
+  href: string;
+  label: string;
+}
+
+interface Props {
+  items: ILink[];
+}
+
+const Breadcrumbs: FC<Props> = ({ items }) => (
   <nav
     aria-label="breadcrumb"
     className="flex flex-row-reverse py-4 px-4 sm:px-6 lg:px-8"
   >
     <ol className="flex leading-none text-indigo-600 divide-x divide-indigo-400">
-      <li className="pr-4 font-medium text-gray-500 hover:text-gray-900">
-        <a href="#Buy">Buy</a>
-      </li>
-      <li className="px-4 font-medium text-gray-500 hover:text-gray-900">
-        <a href="#MyCourses">My Courses</a>
-      </li>
-      <li className="px-4 font-medium text-gray-500 hover:text-gray-900">
-        <a href="#Manage">Manage Courses</a>
-      </li>
+      {items.map((item, i) => (
+        <li
+          key={item.href}
+          className={`${
+            i === 0 ? "pr-4" : "px-4"
+          } font-medium text-gray-500 hover:text-gray-900`}
+        >
+          <Link href={item.href}>
+            <a>{item.label}</a>
+          </Link>
+        </li>
+      ))}
     </ol>
   </nav>
 );
--- components/ui/marketplace/header/index.tsx
+++ components/ui/marketplace/header/index.tsx
@@ -3,11 +3,26 @@ import { FC } from "react";
 import { Breadcrumbs } from "components/ui/common";
 import { EthRates, WalletBar } from "components/ui/web3";
 
+const LINKS = [
+  {
+    href: "/marketplace",
+    label: "Buy",
+  },
+  {
+    href: "/marketplace/courses/owned",
+    label: "My Courses",
+  },
+  {
+    href: "/marketplace/courses/manage",
+    label: "Manage Courses",
+  },
+];
+
 const Header: FC = () => (
   <>
     <WalletBar />
     <EthRates />
-    <Breadcrumbs />
+    <Breadcrumbs items={LINKS} />
   </>
 );
 
