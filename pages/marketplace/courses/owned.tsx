99772999f921ffa3c34a6cd5d29570ba9d5bfe19
Will Urban <willian.urban@gmail.com>
2022-03-20 18:44:38 +0900

    updated mange and owned pages

--- pages/marketplace/courses/owned.tsx
+++ pages/marketplace/courses/owned.tsx
@@ -2,12 +2,16 @@ import { FC } from "react";
 
 import { MarketplaceHeader } from "components/ui/marketplace";
 import { OwnedCourseCard } from "components/ui/course";
+import { Button, Message } from "components/ui/common";
 
 const OwnedCourses: FC = () => (
   <>
     <MarketplaceHeader />
     <section className="grid grid-cols-1">
-      <OwnedCourseCard />
+      <OwnedCourseCard>
+        <Message message="My custom message!" />
+        <Button text="Watch the course" />
+      </OwnedCourseCard>
     </section>
   </>
 );
