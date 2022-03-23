4fb9f5a725b00086253c8e887934cc52abc57d55
Will Urban <willian.urban@gmail.com>
2022-03-23 19:35:22 +0900

    fixed image display inssues on smaller screen in OwnedCourseCard

--- components/ui/course/OwnedCourseCard/index.tsx
+++ components/ui/course/OwnedCourseCard/index.tsx
@@ -20,9 +20,9 @@ const OwnedCourseCard: FC<Props> = ({ children, course }) => {
   const { title, ownedCourseId, proof, price, coverImage, state } = course;
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="flex">
+      <div className="block md:flex">
         <div className="flex-1">
-          <div className="h-full next-image-wrapper">
+          <div className="h-72 md:h-full next-image-wrapper">
             <Image
               className="object-cover"
               src={coverImage}
@@ -49,17 +49,21 @@ const OwnedCourseCard: FC<Props> = ({ children, course }) => {
 
           <div className="border-t border-gray-200">
             <dl>
-              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Course ID</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="bg-gray-50 px-4 py-5 sm:px-6">
+                <span className="text-sm font-medium text-gray-500 mr-1">
+                  Course ID:
+                </span>
+                <span className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   {ownedCourseId}
-                </dd>
+                </span>
               </div>
-              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">Proof</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <div className="bg-white px-4 py-5 sm:px-6">
+                <span className="text-sm font-medium text-gray-500 mr-1">
+                  Proof:
+                </span>
+                <span className="mt-1 text-sm break-words text-gray-900 sm:mt-0 sm:col-span-2">
                   {proof}
-                </dd>
+                </span>
               </div>
               <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
             </dl>
