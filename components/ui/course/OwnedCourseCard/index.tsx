348f229c49e53c383ad502e3714c6a14eb1d0e3a
Will Urban <willian.urban@gmail.com>
2022-03-23 18:08:34 +0900

    improved OwnedCourseCard styles

--- components/ui/course/OwnedCourseCard/index.tsx
+++ components/ui/course/OwnedCourseCard/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { FC } from "react";
 import { IPurchasedCourse } from "utils/types";
 
@@ -6,30 +7,49 @@ interface Props {
 }
 
 const OwnedCourseCard: FC<Props> = ({ children, course }) => {
-  const { title, ownedCourseId, proof } = course;
+  const { title, ownedCourseId, proof, price, coverImage } = course;
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">0.0065 ETH</p>
-      </div>
+      <div className="flex">
+        <div className="flex-1">
+          <div className="h-full next-image-wrapper">
+            <Image
+              className="object-cover"
+              src={coverImage}
+              width="45"
+              height="45"
+              layout="responsive"
+            />
+          </div>
+        </div>
+        <div className="flex-4">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {title}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              {`${price} ETH`}
+            </p>
+          </div>
 
-      <div className="border-t border-gray-200">
-        <dl>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Course ID:</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {ownedCourseId}
-            </dd>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Course ID</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {ownedCourseId}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Proof</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {proof}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
+            </dl>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Proof:</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {proof}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
-        </dl>
+        </div>
       </div>
     </div>
   );
